45d740de41bcaa68f64d5def16a9367e6291ed8b
Jethro Yu <comet.jc@gmail.com>
2018-11-19 19:26:44 +0800

    Show journal details, show bulk edit changes (require modify redmine to add an extra hook: controller_issues_bulk_edit_after_save)

--- lib/hipchat_hooks.rb
+++ lib/hipchat_hooks.rb
@@ -3,74 +3,62 @@ require 'uri'
 require 'json'
 
 class NotificationHook < Redmine::Hook::Listener
+  include IssuesHelper
 
   def controller_issues_new_after_save(context = {})
     issue   = context[:issue]
     project = issue.project
-    return true if !hipchat_configured?(project)
+    return true unless hipchat_configured?(project)
 
-    author  = CGI::escapeHTML(User.current.name)
-    tracker = CGI::escapeHTML(issue.tracker.name.downcase)
-    subject = CGI::escapeHTML(issue.subject)
-    url     = get_url(issue)
-    text    = "#{author} reported #{project.name} #{tracker} #{url}: #{subject}"
+    author    = User.current.name
+    tracker   = issue.tracker.name
+    subject   = issue.subject
+    status    = issue.status.name
+    assignee  = issue.assigned_to.try(:name).presence || "Unassigned"
+    url       = get_url(issue)
+    text      = "#{author} Created #{tracker} ##{issue.id} : #{subject} [ #{status} | #{assignee} ] #{url}"
+    text     += "\n #{issue.description}" unless issue.description.blank?
 
-    data          = {}
-    data[:text]   = text
-    data[:token]  = hipchat_auth_token(project)
-    data[:room_name]   = hipchat_room_name(project)
-    data[:notify] = hipchat_notify(project)
-    data[:endpoint] = hipchat_endpoint(project)
-
-    send_message(data)
+    send_message(project, text)
   end
 
-  def controller_issues_bulk_edit_before_save(context)
-    controller_issues_edit_after_save(context)
+  def controller_issues_bulk_edit_after_save(context = {})
+    controller_issues_edit_after_save({ :params => context[:params], :issue => context[:issue], :journal => context[:issue].current_journal})
   end
 
   def controller_issues_edit_after_save(context = {})
     issue   = context[:issue]
     project = issue.project
-    return true if !hipchat_configured?(project)
+    return true unless hipchat_configured?(project)
 
-    author  = CGI::escapeHTML(User.current.name)
-    tracker = CGI::escapeHTML(issue.tracker.name.downcase)
-    subject = CGI::escapeHTML(issue.subject)
-    comment = context[:journal].respond_to?(:notes) ? CGI::escapeHTML(context[:journal].notes) : ""
-    url     = get_url(issue)
-    text    = "#{author} updated #{project.name} #{tracker} #{url}: #{subject}"
-    text   += ": #{truncate(comment)}" unless comment.blank?
+    editor    = User.current.name
+    tracker   = issue.tracker.name
+    subject   = issue.subject
+    url       = get_url(issue)
+    text      = "#{editor} Update #{tracker} ##{issue.id}: #{subject} #{url}"
 
-    data          = {}
-    data[:text]   = text
-    data[:token]  = hipchat_auth_token(project)
-    data[:room_name]   = hipchat_room_name(project)
-    data[:notify] = hipchat_notify(project)
-    data[:endpoint] = hipchat_endpoint(project)
+    journal = context[:journal]
+    comment   = journal.try(:notes)
+    text     += ": #{truncate(comment)}" if comment.present?
+    details   = journal.visible_details
+    details   = details_to_strings(details, true).map{ |detail| "• #{detail}" }.join("\n") if details.present?
+    text     += "\n#{details}" if details.present?
 
-    send_message(data)
+    send_message(project, text)
   end
 
   def controller_wiki_edit_after_save(context = {})
     page    = context[:page]
     project = page.wiki.project
-    return true if !hipchat_configured?(project)
+    return true unless hipchat_configured?(project)
 
-    author       = CGI::escapeHTML(User.current.name)
-    wiki         = CGI::escapeHTML(page.pretty_title)
-    project_name = CGI::escapeHTML(project.name)
+    author       = User.current.name
+    wiki         = page.pretty_title
+    project_name = project.name
     url          = get_url(page)
-    text         = "#{author} edited #{project_name} wiki page #{url}"
+    text         = "#{author} edited the #{wiki} on #{project_name} #{url}"
 
-    data          = {}
-    data[:text]   = text
-    data[:token]  = hipchat_auth_token(project)
-    data[:room_name]   = hipchat_room_name(project)
-    data[:notify] = hipchat_notify(project)
-    data[:endpoint] = hipchat_endpoint(project)
-
-    send_message(data)
+    send_message(project, text)
   end
 
   private
@@ -82,7 +70,7 @@ class NotificationHook < Redmine::Hook::Listener
           Setting.plugin_redmine_hipchat[:projects].include?(project.id.to_s) &&
           Setting.plugin_redmine_hipchat[:auth_token] &&
           Setting.plugin_redmine_hipchat[:room_name] &&
-	  Setting.plugin_redmine_hipchat[:endpoint]
+          Setting.plugin_redmine_hipchat[:endpoint]
       return true
     else
       Rails.logger.info "Not sending HipChat message - missing config"
@@ -91,18 +79,15 @@ class NotificationHook < Redmine::Hook::Listener
   end
 
   def hipchat_auth_token(project)
-    return project.hipchat_auth_token if !project.hipchat_auth_token.empty?
-    return Setting.plugin_redmine_hipchat[:auth_token]
+    return project.hipchat_auth_token.presence || Setting.plugin_redmine_hipchat[:auth_token]
   end
 
   def hipchat_room_name(project)
-    return project.hipchat_room_name if !project.hipchat_room_name.empty?
-    return Setting.plugin_redmine_hipchat[:room_name]
+    return project.hipchat_room_name.presence || Setting.plugin_redmine_hipchat[:room_name]
   end
 
   def hipchat_endpoint(project)
-    return project.hipchat_endpoint if !project.hipchat_endpoint.empty?
-    return Setting.plugin_redmine_hipchat[:endpoint]
+    return project.hipchat_endpoint.presence || Setting.plugin_redmine_hipchat[:endpoint]
   end
 
   def hipchat_notify(project)
@@ -119,26 +104,25 @@ class NotificationHook < Redmine::Hook::Listener
     end
   end
 
-  def send_message(data)
-    Rails.logger.info "Sending message to HipChat: #{data[:text]}"
-    endpoint = data[:endpoint] || 'api.hipchat.com'
-    room_name = data[:room_name]
-    room_token = data[:token]
-    uri = URI.parse("https://#{endpoint}/v2/room/#{CGI::escape(room_name)}/notification?auth_token=#{room_token}")
+  def send_message(project, text)
+    endpoint = hipchat_endpoint(project) || 'api.hipchat.com'
+    Rails.logger.info "Sending message to HipChat( https://#{endpoint} ): #{text}"
+    room_name = CGI::escape(hipchat_room_name(project))
+    room_token = hipchat_auth_token(project)
+    uri = URI.parse("https://#{endpoint}/v2/room/#{room_name}/notification?auth_token=#{room_token}")
     http = Net::HTTP.new(uri.host, uri.port)
     http.use_ssl = true
     http.verify_mode = OpenSSL::SSL::VERIFY_NONE
 
-	 req = Net::HTTP::Post.new(uri.request_uri)
-	 req.body = {
-        "color"          => 'random',
-        "message"        => data[:text],
+    req = Net::HTTP::Post.new(uri.request_uri)
+    req.body = {
+        "message"        => text,
         "message_format" => 'text',
-        "notify"         => data[:notify] ? true : false
+        "notify"         => hipchat_notify(project) ? true : false
     }.to_json
-   
+
     req['Content-Type'] = 'application/json'
-	 Rails.logger.info "Before HipChat Begin Http.. #{req.body} (#{uri.request_uri}"
+    Rails.logger.info "Before HipChat Begin Http.. #{req.body} (#{uri.request_uri}"
     begin
       res = http.start do |connection|
         connection.request(req)
@@ -146,7 +130,7 @@ class NotificationHook < Redmine::Hook::Listener
     rescue Net::HTTPBadResponse => e
       Rails.logger.error "Error hitting HipChat API: #{e}"
     end
-	 Rails.logger.info "HipChat Result: #{res.body}"
+    Rails.logger.info "HipChat Result: #{res.body}"
   end
 
   def truncate(text, length = 20, end_string = '…')
